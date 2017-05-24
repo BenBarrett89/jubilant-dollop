83f91ee65962ac8bc58384faa8b7b317e393e231
Ben Barrett <benjaminbarrett1989@gmail.com>
2017-05-24 14:00:52 +0100

    Very basic d3 (div and HTML only) seems to be working using react-faux-dom

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -12,7 +12,7 @@ class Home extends React.Component {
         <Row>
           <Col xs={12}>
             <h2><span id={Constants.titleId}>{Constants.titleText}</span></h2>
-            <p>The purpose of this site is to have a go at using <a href='https://d3js.org/'>D3</a> within <a href='https://facebook.github.io/react/'>React</a> and/or with <a href='http://redux.js.org/'>Redux</a></p>
+            <p>The purpose of this site is to have a go at using <a target='_blank' href='https://d3js.org/'>D3</a> within <a target='_blank' href='https://facebook.github.io/react/'>React</a> and/or with <a target='_blank' href='http://redux.js.org/'>Redux</a></p>
             <blockquote>
               <p>React doesn't play very well with D3</p>
               <small>Someone</small>
--- client/components/RotatingVoronoi.jsx
+++ client/components/RotatingVoronoi.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import { Col, Grid, Row, Well } from 'react-bootstrap'
+import { Col, Grid, Row } from 'react-bootstrap'
+import * as d3 from 'd3'
+import ReactFauxDOM from 'react-faux-dom'
 
 import Constants from '../constants/rotating-voronoi-constants'
 
 class RotatingVoronoi extends React.Component {
+  renderRotatingVoronoi () {
+    var el = ReactFauxDOM.createElement('div')
+
+    d3.select(el)
+      .append('div')
+      .html('Hello World!')
+
+    return el.toReact()
+  }
+
   render () {
     return (
       <Grid>
@@ -12,9 +24,8 @@ class RotatingVoronoi extends React.Component {
         <Row>
           <Col xs={12}>
             <h2><span id={Constants.titleId}>{Constants.titleText}</span></h2>
-            <Well>
-              <p>Gonna try some D3 in here</p>
-            </Well>
+            <p>Test to try and get <a target='_blank' href='https://bl.ocks.org/mbostock/4636377'>Mike Bostock's Rotating Voronoi demo</a> working in React</p>
+            {this.renderRotatingVoronoi()}
           </Col>
         </Row>
       </Grid>
